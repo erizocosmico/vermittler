c67d021b9e64b0b648d1c6cad979b1036599aef7
José Miguel Molina Arboledas <rd4091@gmail.com>
2014-01-16 22:38:40 +0100

    Cache was not actually added, now it is

---
+++ config.go
@@ -0,0 +1,20 @@
+package main
+
+// App configuration.
+type Config struct {
+	// Enable or disable the cache
+	CacheEnabled bool
+	// Path where the cache is stored, without trailing slash
+	CachePath string
+	// Port where the app will run preceded e.g: "8888"
+	Port string
+}
+
+// Returns a fancy and very hardcoded config for our app.
+func NewConfig() *Config {
+	return &Config{
+		CacheEnabled: true,
+		CachePath:    "/Users/Mik3/Desktop",
+		Port:         "8888",
+	}
+}
